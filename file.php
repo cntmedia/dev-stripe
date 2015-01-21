75d9b5d20220e93385ec78203bc843a43f5a6d75
x <x@x.com>
2015-01-21 13:16:15 -0800

    added include function for file.php --heroku not including files normally
    Signed-off-by: x <x@x.com>

--- file.php
+++ file.php
@@ -1,3 +1,21 @@
 <?
-echo(dirname(__FILE__) . '/Stripe.php');
+echo(dirname(__FILE__) . '/Stripe.php') . "\n";
+echo(getcwd() . "\n");
+echo(dirname ( realpath ( __FILE__ ) ) );
+
+function get_file_dir() {
+    global $argv;
+    $dir = dirname(getcwd() . '/' . $argv[0]);
+    $curDir = getcwd();
+    chdir($dir);
+    $dir = getcwd();
+    chdir($curDir);
+    return $dir;
+}
+
+include get_file_dir() . '/include.php';
+// or even..
+//chdir(get_file_dir());
+//include './include.php';
+
 ?>
