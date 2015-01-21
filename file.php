17b766feb609d2c3d384b3598f9f3bb2afcd1d3e
x <x@x.com>
2015-01-21 13:19:46 -0800

    added in relative path to '/app/file-name.php'
    Signed-off-by: x <x@x.com>

--- file.php
+++ file.php
@@ -1,4 +1,5 @@
 <?
+/*
 echo(dirname(__FILE__) . '/Stripe.php') . "\n";
 echo(getcwd() . "\n");
 echo(dirname ( realpath ( __FILE__ ) ) );
@@ -17,5 +18,10 @@ include get_file_dir() . '/include.php';
 // or even..
 //chdir(get_file_dir());
 //include './include.php';
+
+*/
+
+include('/app/include.php');
+
 
 ?>
