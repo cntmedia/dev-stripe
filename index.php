9d475f395d888526dfdb46211655a63cc3a060fc
x <x@x.com>
2015-01-22 09:20:02 -0800

    stripe test form
    Signed-off-by: x <x@x.com>

--- index.php
+++ index.php
@@ -1,38 +1,10 @@
 <?php
-
-echo("server_script_filename");
-echo "\r\n";
-echo(_SERVER["SCRIPT_FILENAME"]);
-
-echo("dir_dirname_file");
-echo "\r\n";
-echo(dirname(dirname(__FILE__)));
-
-echo("server_php_self");
-echo "\r\n";
-echo(dirname($_SERVER['PHP_SELF']));
-
-
-
-echo("dir_file");
-echo "\r\n";
-echo(dirname(__FILE__));
-
-
-
-$base_url = str_replace($DOCUMENT_ROOT, "", 
-dirname($PHP_SELF));
-echo("base_url");
-echo "\r\n";
-echo($base_url);
-
-echo("php_self");
-echo "\r\n";
-echo(dirname($PHP_SELF));
-
-
-echo("document_root");
-echo "\r\n";
-echo($DOCUMENT_ROOT);
-
+require(dirname(__FILE__).'/config.php');
 ?>
+
+<form action="charge.php" method="post">
+  <script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+          data-key="<?php echo $stripe['publishable_key']; ?>"
+          data-amount="5000" data-description="One year's subscription"></script>
+</form>
+
--- paths.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo(dirname(__FILE__).'/lib/include.php');
-require(dirname(__FILE__).'/lib/include.php');
-echo "\r\n";
-echo(dirname(__FILE__).'/lib/Stripe/include.php');
-require(dirname(__FILE__).'/lib/Stripe/include.php');
-?>
