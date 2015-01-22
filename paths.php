d8562ed1593cb8627cc45e307baaa26464a1c946
x <x@x.com>
2015-01-22 09:06:03 -0800

    include paths
    Signed-off-by: x <x@x.com>

---
+++ paths.php
@@ -0,0 +1,7 @@
+<?php
+echo(dirname(__FILE__).'/lib/include.php');
+require(dirname(__FILE__).'/lib/include.php');
+echo "\r\n";
+echo(dirname(__FILE__).'/lib/Stripe/include.php');
+require(dirname(__FILE__).'/lib/Stripe/include.php');
+?>
