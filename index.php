fb4b6539508ca0ac099231b67679638202f4961f
x <x@x.com>
2015-01-22 08:59:09 -0800

    added in new lines for reading
    Signed-off-by: x <x@x.com>

--- index.php
+++ index.php
@@ -1,21 +1,38 @@
-<?
-echo("server_script_filename"."\n");
+<?php
+
+echo("server_script_filename");
+echo "\r\n";
 echo(_SERVER["SCRIPT_FILENAME"]);
-echo("dir_dirname_file"."\n");
+
+echo("dir_dirname_file");
+echo "\r\n";
 echo(dirname(dirname(__FILE__)));
-echo("server_php_self"."\n");
+
+echo("server_php_self");
+echo "\r\n";
 echo(dirname($_SERVER['PHP_SELF']));
-echo("dir_file"."\n");
+
+
+
+echo("dir_file");
+echo "\r\n";
 echo(dirname(__FILE__));
+
 
 
 $base_url = str_replace($DOCUMENT_ROOT, "", 
 dirname($PHP_SELF));
-echo("base_url"."\n");
+echo("base_url");
+echo "\r\n";
 echo($base_url);
-echo("php_self"."\n");
+
+echo("php_self");
+echo "\r\n";
 echo(dirname($PHP_SELF));
-echo("document_root"."\n");
+
+
+echo("document_root");
+echo "\r\n";
 echo($DOCUMENT_ROOT);
 
 ?>
